7a9aa6f64a63d1aea6624d56b5dacce5f7082954
Marissa Miner <marissaminer@Marissas-MacBook-Air.local>
2018-04-20 22:13:13 -0500

    added CSS, still working on scoring

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 var q1 = {
     q: "Chicago has 77 neighborhoods.",
     answerChoices: ["True", "False"],
-    correct: "True",
+    // correct: "True",
     name: "question1",
     image: "assets/images/aerialview.jpg"
 };
@@ -21,7 +21,7 @@ var q1 = {
 var q2 = {
     q: "Which of the following colors is not one of the El train lines?",
     answerChoices: ["Purple", "Green", "Red", "White"],
-    correct: "White",
+    // correct: "White",
     name: "question2",
     image: "assets/images/brownline.jpg"
 }
@@ -29,7 +29,7 @@ var q2 = {
 var q3 = {
     q: "Northwestern University is located in which Chicago suburb?",
     answerChoices: ["Evanston", "Wilmette", "Skokie", "Mt Prospect"],
-    correct: "Evanston",
+    // correct: "Evanston",
     name: "question3",
     image: "assets/images/northwestern.jpg"
 }
@@ -37,7 +37,7 @@ var q3 = {
 var q4 = {
     q: "The Cloud Gate statue in downtown Chicago is nicknamed The Bean.",
     answerChoices: ["True", "False"],
-    correct: "True",
+    // correct: "True",
     name: "question4",
     image: "assets/images/the-bean.jpg"
 }
@@ -45,7 +45,7 @@ var q4 = {
 var q5 = {
     q: "What is the tallest building in Chicago?",
     answerChoices: ["Hancock Tower", "Willis Tower, aka Sears Tower", "Aqua Tower", "Trump Tower"],
-    correct: "Willis Tower, aka Sears Tower",
+    // correct: "Willis Tower, aka Sears Tower",
     name: "question5",
     image: "assets/images/skyline.jpg"
 }
@@ -53,7 +53,7 @@ var q5 = {
 var q6 = {
     q: "Chicago is located along Lake Superior.",
     answerChoices: ["True", "False"],
-    correct: "False",
+    // correct: "False",
     name: "question6",
     image: "assets/images/alongthelake.jpg"
 }
@@ -61,7 +61,7 @@ var q6 = {
 var q7 = {
     q: "Lincoln Park Zoo is one of the few free-admission zoos in the United States.",
     answerChoices: ["True", "False"],
-    correct: "True",
+    // correct: "True",
     name: "question7",
     image: "assets/images/lincolnparkzoo.jpg"
 }
@@ -69,7 +69,7 @@ var q7 = {
 var q8 = {
     q: "Former President Barack Obama lived in which Chicago neighborhood?",
     answerChoices: ["Rogers Park", "Hyde Park", "Jefferson Park", "Logan Square"],
-    correct: "Hyde Park",
+    // correct: "Hyde Park",
     name: "question8",
     image: "assets/images/obamashouse.jpg"
 }
@@ -141,14 +141,19 @@ var questions = [q1, q2, q3, q4, q5, q6, q7, q8];
         for (var i = 0; i < questions.length; i++) {
             var correctAnswer = questions[i].correct;
             var questionName = questions[i].name;
-            var selected = $('input[type="radio"][name=' + questionName + ']:checked').val();
-            if (selected === undefined) {
-                noAnswer++;
-            } else if (selected === correctAnswer) {
-                right++;
-            } else {
-                wrong++;
-            }
+            if ($("input[name=q1]:checked").val() === "correct"){
+                right++; 
+                } else {
+                    wrong++;
+                }
+            // var selected = $('input[type="radio"][name=' + questionName + ']:checked').val();
+            // if (selected === undefined) {
+            //     noAnswer++;
+            // } else if (selected === correctAnswer) {
+            //     right++;
+            // } else {
+            //     wrong++;
+            // }
         }
     }
 
@@ -158,8 +163,6 @@ var questions = [q1, q2, q3, q4, q5, q6, q7, q8];
         $("#theForm").hide();
         $("#result-page").show();
         $("#result-page").html("<button id='game_over'>" + "Game Over" + "</button>");
-        //if desired throw in a pause
-        // $('#yes-audio').trigger("pause");
         stop();
     }
 
@@ -170,12 +173,10 @@ var questions = [q1, q2, q3, q4, q5, q6, q7, q8];
         $("#result-page").append("<h4>" + "incorrect answers: " + wrong + "</h4>");
         $("#result-page").append("<h4>" + "missed questions: " + noAnswer+ "</h4>");
         if(right > 3){
-            $("#result-page").append("<h2>" + "You are a champ" + "</h2>");
-            $("#result-page").append("<img src='assets/images/mario.jpeg'>");
+            $("#result-page").append("<h2>" + "Awesome!" + "</h2>");
 
         } else {
-            $("#result-page").append("<h2>" + "Take a left and get me to my destination" + "</h2>");
-            $("#result-page").append("<img src='assets/images/pig.jpeg'>");
+            $("#result-page").append("<h2>" + "Better luck  next time!" + "</h2>");
 
         }
 
